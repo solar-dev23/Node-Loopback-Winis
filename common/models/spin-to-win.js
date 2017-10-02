12a1dc324565923ef5e551ad0aeefb439535b05b
David Tabachnikov <david@winisapp.com>
2017-10-02 17:43:07 +0200

    Support spin-to-win and basic login

--- common/models/spin-to-win.js
+++ common/models/spin-to-win.js
@@ -1,17 +1,47 @@
 'use strict';
+const Ivoire = require('ivoire-weighted-choice');
 
-class SpinToWin {
-  constructor(userId) {
-    this.userId = userId;
-  }
+const spinOptions = [
+  'diamond', 'present', 'double_spin', '5_winis', 'double_diamond',
+  'empty', '2_winis', 'double_scratch', 'scratch', 'spin',
+];
 
-  calculateSpin() {
-    
-  }
+const spinWeights = [
+  7, 1, 5, 30, 3,
+  1, 33, 5, 10, 5,
+];
 
-  spin(options) {
-    // this.userId = options.accessToken.id;
-  }
-}
+module.exports = function(SpinToWin) {
+  SpinToWin.calculateSpin = () => {
+    const ivoire = new Ivoire();
+    return ivoire.weighted_choice(spinOptions, spinWeights);
+  };
 
-module.exports = SpinToWin;
+  SpinToWin.spin = async (options) => {
+    const spinResult = SpinToWin.calculateSpin();
+    const token = options && options.accessToken;
+    const userId = token && token.userId;
+    const UserModel = SpinToWin.app.models.user;
+
+    const user = await UserModel.findById(userId);
+    if (user.spins == 0) {
+      throw new Error('User has no spins');
+    }
+
+    const spin = await SpinToWin.create({
+      spinResult: spinResult,
+      userId: user.id,
+    });
+
+    user.handlePrize(spinResult);
+    user.spins--;
+
+    await UserModel.save(user);
+
+    return {
+      spinResult: spinResult,
+      spin: spin,
+      user: user
+    };
+  };
+};
--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const debug = require('debug')('Winis:UserModel');
+
 module.exports = function(User) {
   delete User.validations.email;
+  User.prototype.handlePrize = function(prize) {
+    debug(`Handle prize ${prize}`);
+
+    switch(prize) {
+      case 'diamond':
+        this.diamonds++;
+        break;
+      case 'present':
+        debug('Need to handle present');
+        break;
+      case 'double_spin':
+        this.spins += 2;
+        break;
+      case '5_winis':
+        this.winis += 5;
+        break;
+      case 'double_diamond':
+        this.diamonds += 2;
+        break;
+      case '2_winis':
+        this.winis += 2;
+        break;
+      case 'double_scratch':
+        this.scratches += 2;
+        break;
+      case 'scratch':
+        this.scratch++;
+        break;
+      case 'spin':
+        this.spins++;
+        break;
+      default:
+        break;
+    }
+  };
 };
