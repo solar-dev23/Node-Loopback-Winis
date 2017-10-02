81df75d69c62cce53944b201c52731214ad8a6f2
David Tabachnikov <davidt@scholarshipowl.com>
2017-10-02 13:38:24 +0200

    Initial commit

---
+++ common/models/spin-to-win.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class SpinToWin {
+  constructor(userId) {
+    this.userId = userId;
+  }
+
+  calculateSpin() {
+    
+  }
+
+  spin(options) {
+    // this.userId = options.accessToken.id;
+  }
+}
+
+module.exports = SpinToWin;
