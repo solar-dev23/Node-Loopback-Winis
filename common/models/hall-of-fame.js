9bc3dbbf5861ba9656653d7cd096da00fd7b7305
David Tabachnikov <david@winisapp.com>
2017-10-06 18:43:03 +0200

    Add support for the list of hall-of-fame

--- common/models/hall-of-fame.js
+++ common/models/hall-of-fame.js
@@ -1,5 +1,29 @@
 'use strict';
 
-module.exports = function(Halloffame) {
+module.exports = function(HallOfFame) {
+  HallOfFame.findTopScores = async function(whereQuery) {
+    const UserModel =  HallOfFame.app.models.user;
 
+    return await UserModel.find({
+      where: whereQuery, limit: 20, order: 'winis DESC',
+      fields: { username: true, winis: true, id: true, avatar: true }
+    });
+  };
+
+  HallOfFame.getTopScores = async function() {
+    return HallOfFame.findTopScores({});
+  };
+
+  HallOfFame.getFriendsTopScores = async function(options) {
+    const token = options && options.accessToken;
+    const userId = token && token.userId;
+    const UserModel = HallOfFame.app.models.user;
+
+    const user = await UserModel.findById(userId);
+    const friendIds = user.friendIds;
+
+    const whereQuery = {inq: friends};
+
+    return HallOfFame.findTopScores(whereQuery);
+  }
 };
