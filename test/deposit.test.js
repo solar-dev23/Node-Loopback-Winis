3a93a9e9acedfc58a79ea426c5cc19c434e99fc8
David Tabachnikov <davidt@scholarshipowl.com>
2018-02-28 17:28:43 +0100

    Clean the tests and rename the rewardsConfig endpoint

--- test/deposit.test.js
+++ test/deposit.test.js
@@ -8,7 +8,7 @@ const md5 = require('md5');
 
 describe('Deposit', function() {
   let accessToken, DepositModel;
-  let ownerUser, tapjoyKey;
+  let ownerUser;
 
   beforeEach(async function() {
     DepositModel = app.models.deposit;
@@ -16,12 +16,11 @@ describe('Deposit', function() {
     await UserModel.deleteAll();
 
     ownerUser = await UserModel.create({
-      winis: 0, 
-      spins: 0, 
-      diamonds: 0, 
+      winis: 0,
+      spins: 0,
+      diamonds: 0,
       scratchs: 0,
     });
-    tapjoyKey = app.get('tapjoyKey');
     accessToken = await ownerUser.createAccessToken();
   });
 
@@ -33,6 +32,8 @@ describe('Deposit', function() {
     let grantedWinis = 25;
     let displayMultiplier = 1.0;
     let randomExternalId = 456;
+    const tapjoyKey = app.get('tapjoyKey');
+
     it('should award a certain amount of winis for completing a mission', function(done) {
       const params = {
         'id': randomExternalId,
@@ -120,157 +121,156 @@ describe('Deposit', function() {
   });
 
   describe('appStore', function() {
-    beforeEach(async function() {
-      DepositModel.getRewardConfiguration = async function() {
-        return [
+    app.models.Deposit.getRewardConfiguration = async function() {
+      return [
         {productId: '2_winis', iconId: 'winis', amount: 2},
         {productId: '5_scratches', iconId: 'scratch', amount: 5},
         {productId: '7_diamonds', iconId: 'diamond', amount: 7},
         {productId: '3_presents', iconId: 'present', amount: 3},
         {productId: '12_spins', iconId: 'spin', amount: 12},
-        ];
-      };
-    });
+      ];
+    };
 
     it('should return rewardConfiguration', function(done) {
       request
-      .get('/api/deposits/getRewardConfiguration')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send()
-      .then((res)=>{
-        expect(res.body[0].productId).to.be.equal('2_winis');
-        expect(res.body[0].iconId).to.be.equal('winis');
-        expect(res.body[0].amount).to.be.equal(2);
-        done();
-      });
+        .get('/api/deposits/appStore/rewardsConfig')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send()
+        .then((res)=>{
+          expect(res.body[0].productId).to.be.equal('2_winis');
+          expect(res.body[0].iconId).to.be.equal('winis');
+          expect(res.body[0].amount).to.be.equal(2);
+          done();
+        });
     });
 
     it('should award 2 winis', function(done) {
-      const params = {        
+      const params = {
         'method': 'appstore',
         'externalId': '2_winis',
       };
 
       request
-      .post('/api/deposits/appStore/')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send(params)
-      .then((res)=>{
-        expect(res.body.success).to.be.equals(true);
-        expect(res.body.method).to.be.equals('appstore');
-        expect(res.body.user.winis).to.be.equal(2);
-        expect(res.body.user.diamonds).to.be.equal(0);
-        expect(res.body.user.spins).to.be.equal(0);
-        expect(res.body.user.scratches).to.be.equal(0);
-        done();
-      });
+        .post('/api/deposits/appStore')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send(params)
+        .then((res)=>{
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.method).to.be.equals('appstore');
+          expect(res.body.user.winis).to.be.equal(2);
+          expect(res.body.user.diamonds).to.be.equal(0);
+          expect(res.body.user.spins).to.be.equal(0);
+          expect(res.body.user.scratches).to.be.equal(0);
+          done();
+        });
     });
 
     it('should award 5 scratches', function(done) {
-      const params = {        
+      const params = {
         'externalId': '5_scratches',
       };
 
       request
-      .post('/api/deposits/appStore/')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send(params)
-      .then((res)=>{
-        expect(res.body.success).to.be.equals(true);
-        expect(res.body.method).to.be.equals('appstore');
-        expect(res.body.user.winis).to.be.equal(0);
-        expect(res.body.user.diamonds).to.be.equal(0);
-        expect(res.body.user.spins).to.be.equal(0);
-        expect(res.body.user.scratches).to.be.equal(5);
-        done();
-      });
+        .post('/api/deposits/appStore')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send(params)
+        .then((res)=>{
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.method).to.be.equals('appstore');
+          expect(res.body.user.winis).to.be.equal(0);
+          expect(res.body.user.diamonds).to.be.equal(0);
+          expect(res.body.user.spins).to.be.equal(0);
+          expect(res.body.user.scratches).to.be.equal(5);
+          done();
+        });
     });
 
     it('should award 7 diamonds', function(done) {
-      const params = {       
+      const params = {
         'method': 'appstore',
         'externalId': '7_diamonds',
       };
 
       request
-      .post('/api/deposits/appStore/')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send(params)
-      .then((res)=>{
-        expect(res.body.success).to.be.equals(true);
-        expect(res.body.method).to.be.equals('appstore');
-        expect(res.body.user.winis).to.be.equal(0);
-        expect(res.body.user.diamonds).to.be.equal(7);
-        expect(res.body.user.spins).to.be.equal(0);
-        expect(res.body.user.scratches).to.be.equal(0);
-        done();
-      });
+        .post('/api/deposits/appStore')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send(params)
+        .then((res)=>{
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.method).to.be.equals('appstore');
+          expect(res.body.user.winis).to.be.equal(0);
+          expect(res.body.user.diamonds).to.be.equal(7);
+          expect(res.body.user.spins).to.be.equal(0);
+          expect(res.body.user.scratches).to.be.equal(0);
+          done();
+        });
     });
 
     it('should award 3 presents', function(done) {
-      const params = {      
+      const params = {
         'method': 'appstore',
         'externalId': '3_presents',
       };
-      
+
       request
-      .post('/api/deposits/appStore/')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send(params)
-      .then((res)=>{
-        expect(res.body.success).to.be.equals(true);
-        expect(res.body.method).to.be.equals('appstore');
-        expect(res.body.user.winis).to.be.equal(30);
-        expect(res.body.user.diamonds).to.be.equal(3);
-        expect(res.body.user.spins).to.be.equal(3);
-        expect(res.body.user.scratches).to.be.equal(3);
-        done();
-      });
+        .post('/api/deposits/appStore')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send(params)
+        .then((res)=>{
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.method).to.be.equals('appstore');
+          expect(res.body.user.winis).to.be.equal(30);
+          expect(res.body.user.diamonds).to.be.equal(3);
+          expect(res.body.user.spins).to.be.equal(3);
+          expect(res.body.user.scratches).to.be.equal(3);
+          done();
+        });
     });
 
     it('should award 12 spins', function(done) {
-      const params = {       
+      const params = {
         'method': 'appstore',
         'externalId': '12_spins',
       };
 
       request
-      .post('/api/deposits/appStore/')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send(params)
-      .then((res)=>{
-        expect(res.body.success).to.be.equals(true);
-        expect(res.body.method).to.be.equals('appstore');
-        expect(res.body.user.winis).to.be.equal(0);
-        expect(res.body.user.diamonds).to.be.equal(0);
-        expect(res.body.user.spins).to.be.equal(12);
-        expect(res.body.user.scratches).to.be.equal(0);
-        done();
-      });
+        .post('/api/deposits/appStore')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send(params)
+        .then((res)=>{
+          expect(res.body.success).to.be.equals(true);
+          expect(res.body.method).to.be.equals('appstore');
+          expect(res.body.user.winis).to.be.equal(0);
+          expect(res.body.user.diamonds).to.be.equal(0);
+          expect(res.body.user.spins).to.be.equal(12);
+          expect(res.body.user.scratches).to.be.equal(0);
+          done();
+        });
     });
 
     it('should fail award wrong externalId', function(done) {
       const unmute = mute();
-      const params = {       
+      const params = {
         'method': 'appstore',
         'externalId': '3_winis',
       };
+
       request
-      .post('/api/deposits/appStore/')
-      .set('Authorization', accessToken.id)
-      .expect('Content-Type', /json/)
-      .send(params)
-      .then((res)=>{
-        expect(res.statusCode).to.be.equals(422);
-        unmute();
-        done();
-      });
+        .post('/api/deposits/appStore')
+        .set('Authorization', accessToken.id)
+        .expect('Content-Type', /json/)
+        .send(params)
+        .then((res)=>{
+          expect(res.statusCode).to.be.equals(422);
+          unmute();
+          done();
+        });
     });
   });
 });
