4896078ac31685289c938443823ee93eee91a069
David Tabachnikov <davidt@scholarshipowl.com>
2018-06-08 02:17:59 +0300

    Implement competition 'nearest' logic

--- common/models/competition.js
+++ common/models/competition.js
@@ -1,13 +1,35 @@
 'use strict';
 
+const moment = require('moment');
+const momenttz = require('moment-timezone');
+
 module.exports = function(Competition) {
+  Competition.getStartOfDay = function(timezone) {
+    return momenttz(new Date()).tz(timezone).startOf('day').valueOf();
+  };
+
   /**
    * Return the current or next competition
    * @param {Function(Error, object)} callback
    */
+  Competition.nearest = async function() {
+    const startOfCurrentDay = Competition.getStartOfDay('UTC');
+    const competition = await Competition.findOne({where: {endDate: {gt: startOfCurrentDay}}});
+    let status;
 
-  Competition.next = async function(callback) {
-    const competition = await Competition.findOne({order: 'endDate ASC'});
-    callback(null, competition);
+    if (competition) {
+      if (moment(competition.startDate).isBefore(startOfCurrentDay)) {
+        status = 'running';
+      } else {
+        status = 'pending';
+      }
+    } else {
+      status = 'empty';
+    }
+
+    return {
+      status: status,
+      competition: competition
+    }
   };
 };
--- test/competition.test.js
+++ test/competition.test.js
@@ -5,11 +5,13 @@ const expect = require('chai').expect;
 const request = require('supertest')(app);
 const moment = require('moment-timezone');
 
+let CompetitionModel;
+
 describe('Competition', function() {
   beforeEach(async function() {
-    const CompetitionModel = app.models.competition;
+    CompetitionModel = app.models.competition;
     await CompetitionModel.deleteAll();
-    const [competition1, competition2] = await CompetitionModel.create([
+    const [competition1] = await CompetitionModel.create([
       {
         prize: 'iPhone X',
         startDate: moment().add(7, 'days'),
@@ -23,16 +25,41 @@ describe('Competition', function() {
     await app.dataSources.db.connector.disconnect();
   });
 
-  it('should return the next scheduled competition', function() {
+  it('should return the next scheduled competition', function(done) {
     request
-      .get('/competitions/next')
-      .send()
+      .get('/api/competitions/nearest')
       .then((res) => {
-        expect()
+        const status = res.body.status;
+        expect(status).to.be.equal('pending');
+        done();
       });
   });
 
-  it('should return the currently running competition', function() {
+  it('should return the currently running competition', function(done) {
+    CompetitionModel.getStartOfDay = function() {
+      return moment(new Date()).tz('UTC').startOf('day').add(8, 'days');
+    };
 
+    request
+      .get('/api/competitions/nearest')
+      .then((res) => {
+        const status = res.body.status;
+        expect(status).to.be.equal('running');
+        done();
+      });
+  });
+
+  it('should return nothing after the last competition', function(done) {
+    CompetitionModel.getStartOfDay = function() {
+      return moment(new Date()).tz('UTC').startOf('day').add(15, 'days');
+    };
+
+    request
+      .get('/api/competitions/nearest')
+      .then((res) => {
+        const status = res.body.status;
+        expect(status).to.be.equal('empty');
+        done();
+      });
   });
 });
