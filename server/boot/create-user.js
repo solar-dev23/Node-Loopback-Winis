1346573e7c025e5c0dfb84536f7b143d4fdda2a6
solar-dev23 <solar.dev023@gmail.com>
2018-07-24 18:58:19 +0200

    Add docker-compose functionality

---
+++ server/boot/create-user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(app) {
+  if (process.env.NODE_ENV !== 'docker') return;
+
+  console.log('Creating a sample admin');
+
+  app.models.user.create({
+    username: 'admin',
+    password: '123456',
+    isAdmin: true,
+  });
+};
