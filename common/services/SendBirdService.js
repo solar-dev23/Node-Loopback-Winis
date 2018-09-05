80f77d2ddb6c2c78597c6c3262b50080b591a43b
Raphael Aloi <ahadortiz@outlook.com>
2018-09-05 12:26:33 -0300

    Change sendbird API key

--- common/services/SendBirdService.js
+++ common/services/SendBirdService.js
@@ -1,7 +1,8 @@
-
 const request = require('request');
 const sha256 = require('js-sha256');
 const SENDBIRD_AUTH = '7a50d0099c241a4a975fad55ceade7f8be020010';
+const SENDBIRD_APP_ID = '5D66EEA7-201A-4BF4-A931-F35F0B72C5AC';
+const SENDBIRD_API_TOKEN = 'ab0621d91cb3a8a62fda7761c1b4a08c72ec5914';
 const SENDBIRD_API = 'https://api.sendbird.com/';
 
 /**
@@ -9,23 +10,25 @@ const SENDBIRD_API = 'https://api.sendbird.com/';
  * @constructor
  * */
 function SendBirdService() {
-  this.getMessages = function (channelUrl, next) {
+  this.getMessages = (channelUrl, next) => {
     /* eslint-disable camelcase */
-    request
-      .post({
+    request.post(
+      {
         url: `${SENDBIRD_API}admin/read_messages`,
         json: {
           auth: SENDBIRD_AUTH,
           channel_url: channelUrl,
         },
-      }, (err, response, body) => next(err, body));
+      },
+      (err, response, body) => next(err, body)
+    );
     /* eslint-enable camelcase */
   };
 }
 
 module.exports = SendBirdService;
-module.exports.createSignature = (body) => {
-  const hash2 = sha256.hmac.update(SENDBIRD_AUTH, body);
+module.exports.createSignature = body => {
+  const hash2 = sha256.hmac.update(SENDBIRD_API_TOKEN, body);
 
   return hash2.hex();
 };
